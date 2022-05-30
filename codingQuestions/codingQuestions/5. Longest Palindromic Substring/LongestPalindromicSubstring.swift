a28cc6bddf5ce6d915eb58e62cb89a030999efc4
HeitorKun <60748906+HeitorKun@users.noreply.github.com>
2022-05-30 10:53:10 -0300

    started LongestPalindromicSubstring

---
+++ codingQuestions/codingQuestions/5. Longest Palindromic Substring/LongestPalindromicSubstring.swift
@@ -0,0 +1,21 @@
+//
+//  LongestPalindromicSubstring.swift
+//  codingQuestions
+//
+//  Created by Heitor Feijó Kunrath on 27/05/22.
+//
+
+import Foundation
+
+/*
+ Given a string s, return the longest palindromic substring in s.
+ */
+
+class LongestPalindromicSubstring {
+    func longestPalindrome(_ s: String) -> String {
+        
+
+
+        return ""
+    }
+}
